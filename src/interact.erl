59fa3f768a940ee16b9c2c68ac027da24f5db636
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-02-19 12:59:14 +0100

    Boiler plate code edited.

--- src/interact.erl
+++ src/interact.erl
@@ -17,7 +17,7 @@
 % limitations under the License.
 
 -module( interact ).
--author( "Jörgen Brandt <brandjoe@hu-berlin.de>" ).
+-author( "Jorgen Brandt <brandjoe@hu-berlin.de>" ).
 
 -behaviour( effi ).
 
