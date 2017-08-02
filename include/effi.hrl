c5441e457638e18d85f61a556533f10a25bb253a
Jorgen Brandt <joergen.brandt@onlinehome.de>
2017-08-02 14:43:53 +0200

    Version set to 0.1.3, Perl and R support removed.

--- include/effi.hrl
+++ include/effi.hrl
@@ -18,5 +18,5 @@
 
 -define( BUF_SIZE, 1024 ).
 -define( MSG, "<MSG>" ).
--define( BUILD, "2016-11-23" ).
--define( VSN, "0.1.2-snapshot" ).
+-define( BUILD, "2017-08-02" ).
+-define( VSN, "0.1.3" ).
