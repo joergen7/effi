5d4c89edb70dea1b35c54e821878c09c5c7eb3ad
Jörgen Brandt <joergen@cuneiform-lang.org>
2023-03-20 16:49:28 +0100

    remove boiler plate

--- src/effi.erl
+++ src/effi.erl
@@ -2,7 +2,7 @@
 %%
 %% Effi: Erlang foreign function interface
 %%
-%% Copyright 2015-2019 Jörgen Brandt <joergen@cuneiform-lang.org>
+%% Copyright 2015 Jörgen Brandt <joergen@cuneiform-lang.org>
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -18,8 +18,7 @@
 %%
 %% -------------------------------------------------------------------
 %% @author Jörgen Brandt <joergen@cuneiform-lang.org>
-%% @version 0.1.7
-%% @copyright 2015-2019
+%% @copyright 2015
 %%
 %% @doc The standalone application entry point is {@link main/1}. 
 %% The create_port callback defined here is an abstract way to execute child 
@@ -60,7 +59,7 @@
 %% Definitions
 %%====================================================================
 
--define( VSN, "0.1.7" ).
+-define( VSN, "0.1.8" ).
 -define( BUF_SIZE, 1024 ).
 
 
@@ -326,7 +325,7 @@ get_banner() ->
       "  _W   y @  # qF     languages (e.g., Bash, Python, or R) by specifying the",
       "  ^^^^^  P qF  `     function's arguments, body and output values.",
       "",
-      "Copyright 2015-2019 Jorgen Brandt <joergen@cuneiform-lang.org>"
+      "Copyright 2015 Jorgen Brandt <joergen@cuneiform-lang.org>"
 
     ], "\n" ).
 
--- src/effi_octave.erl
+++ src/effi_octave.erl
@@ -2,7 +2,7 @@
 %%
 %% Effi: Erlang foreign function interface
 %%
-%% Copyright 2015-2019 Jörgen Brandt <joergen@cuneiform-lang.org>
+%% Copyright 2015 Jörgen Brandt <joergen@cuneiform-lang.org>
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -18,8 +18,7 @@
 %%
 %% -------------------------------------------------------------------
 %% @author Jörgen Brandt <joergen@cuneiform-lang.org>
-%% @version 0.1.7
-%% @copyright 2015-2019
+%% @copyright 2015
 %%
 %% @doc The standalone application entry point is {@link main/1}. 
 %% The create_port callback defined here is an abstract way to execute child 
